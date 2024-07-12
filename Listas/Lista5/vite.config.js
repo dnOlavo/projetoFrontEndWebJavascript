38888b2e6786ee561e32b628844e2242959a2e5c
dnOlavo <dn.olavo@gmail.com>
2024-07-11 22:19:53 -0300

    att

--- Listas/Lista5/vite.config.js
+++ Listas/Lista5/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projetoFrontEndWebJavascript/Listas/Lista5/dist/index.html"
+  base: "/projetoFrontEndWebJavascript/Listas/Lista5"
 })
